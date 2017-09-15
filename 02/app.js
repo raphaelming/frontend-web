5dfc30f379732ca257c97a5aefa29227d259a457
Lming1 <spear131121@gmail.com>
2017-09-15 17:18:15 +0900

    app modified

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,36 @@
-var str = 'hello world';
-console.log(str);
+// var str = 'hello world';
+// // console.log(str);
+//
+//
+// var isMan = true;
+//
+// if (isMan)
+// {
+//   console.log('남자다');
+// }
+// // else if (true) {
+// //
+// // }
+// else
+// {
+//   console.log('여자다');
+// }
+//
+//
+// for(var count=0; count < 5; count++){
+//
+// }
+
+// function printHello(name){
+//   console.log('hello'+name);
+// }
+
+// printHello('world');
+
+
+var person = {
+  name : 'ming',
+  age : 30,
+  
+}
+console.log(person);
