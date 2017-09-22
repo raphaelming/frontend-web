58798daf8f84c5f2c0ba220ed8dc4de8beaa38eb
Lming1 <spear131121@gmail.com>
2017-09-22 17:44:43 +0900

    event add

--- 03/app.js
+++ 03/app.js
@@ -13,10 +13,45 @@
 // console.log(a.length, a[0], a[1]);
 
 
-var div = document.createElement('div');
-div.style.border = "1px solid red";
-div.innerHTML = "hello!!";
-document.body.appendChild(div); // string x
+// var div = document.createElement('div');
+// div.style.border = "1px solid red";
+// div.innerHTML = "hello!!";
+// document.body.appendChild(div); // string x
+//
+// log.innerHTML = log.innerHTML + '<div style="color:red">hello</div>';
+// log.innerHTML += '<div style="color:red">hello</div>';
 
-log.innerHTML = log.innerHTML + '<div style="color:red">hello</div>';
-log.innerHTML += '<div style="color:red">hello</div>';
+
+// window.onload=function(){
+//     alert("load");
+// };
+
+// window.addEventListener('load', function(){
+//     console.log('load');
+// });
+//
+// window.addEventListener('load', function(){
+//     console.log('안녕??');
+// });
+
+// var log = document.getElementById('log');
+// function printLog(event)
+// {
+//   console.log('log!!!!!!!!!!!!!!');
+//   // debugger;
+// }
+// // log.addEventListener('mouseover', printLog)
+
+var divs = document.querySelectorAll('#wrap div');
+// console.log(divs);
+function changeBg(event){
+  console.log('changeBg');
+  console.log(event.currentTarget);
+  event.currentTarget.style.backgroundColor = 'blue';
+  // divs[i].style.backgroundColor = 'blue';
+
+}
+for(var i=0; i<divs.length; i++){
+divs[i].addEventListener('click', changeBg)
+}
+// window.addEventListener('click',)
