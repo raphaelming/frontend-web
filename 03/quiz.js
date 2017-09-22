aeb8f752bb5deba4802d81a7709264eb0a54ab67
Lming1 <spear131121@gmail.com>
2017-09-22 18:50:13 +0900

    quiz end1

--- 03/quiz.js
+++ 03/quiz.js
@@ -16,36 +16,4 @@ function changeBg(event){
 
 for(var i=0; i<spans.length; i++){
 spans[i].addEventListener('click', changeBg);
-
   }
-
-
-
-
-
-// var spans = document.querySelectorAll('#board span');
-//
-// // console.log(divs);
-// var bTarget = spans[0];
-// var bTargetColor = 'black';
-//
-// function changeBg(event) {
-//
-//   bTarget.style.backgroundColor = bTargetColor;
-//   bTargetColor = event.currentTarget.style.backgroundColor;
-//   event.currentTarget.style.backgroundColor = 'red';
-//   bTarget = event.currentTarget;
-// }
-//
-// for (var i = 0; i < spans.length; i++) {
-//   if (i % 2 == 0 && parseInt(i / 4) % 2 == 0) {
-//     spans[i].style.backgroundColor = 'black';
-//   } else if (i % 2 == 1 && parseInt(i / 4) % 2 == 0) {
-//     spans[i].style.backgroundColor = 'white';
-//   } else if (i % 2 == 0 && parseInt(i / 4) % 2 == 1) {
-//     spans[i].style.backgroundColor = 'white';
-//   } else {
-//     spans[i].style.backgroundColor = 'black';
-//   }
-//   spans[i].addEventListener('click', changeBg);
-// }
