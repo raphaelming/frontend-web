713c11605df278f9e39d2319efaad6fcfb94ee8b
Lming1 <spear131121@gmail.com>
2017-10-08 17:29:37 +0900

    input id 가져오기

--- 04/todo-webapp-0.1/todos.js
+++ 04/todo-webapp-0.1/todos.js
@@ -1,3 +1,10 @@
 //start...
 //연결 확인
 console.log('check todos.js');
+function getDom(id)
+{
+  return document.getElementById(id);
+}
+var getTodoString = getDom('todoString');
+
+console.log(getTodoString);
