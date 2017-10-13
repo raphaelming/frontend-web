8c77f5e1f86127e6c39f787f61e93d2cd0eb65f5
Lming1 <spear131121@gmail.com>
2017-10-13 16:45:37 +0900

    05 modified

--- 05/ajax.js
+++ 05/ajax.js
@@ -9,6 +9,12 @@ function load() {
   console.log('load');
   var req = new XMLHttpRequest();
 
-  req.open('GET', 'dummy', true);
+  req.open('GET', 'dummy.json', true);
   console.log(req);
+  req.onreadystatechange = function() {
+    if(req.readyState === 4){
+      console.log(req.status);
+    }
+  }
+  req.send();
 }
