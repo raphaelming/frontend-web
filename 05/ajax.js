a840dfc80d31bf70b14624a6a569b8943f399f7d
Lming1 <spear131121@gmail.com>
2017-10-13 18:20:06 +0900

    중간저장

--- 05/ajax.js
+++ 05/ajax.js
@@ -1,24 +1,49 @@
-console.log('ajax');
+console.log('ajax.js')
 
 var btn = document.getElementById('btn');
 
-
 btn.addEventListener('click', load);
 
-function load() {
-  console.log('load');
-  var req = new XMLHttpRequest();
 
-  req.open('GET', 'dummy.json', true);
-  console.log(req);
-  req.onreadystatechange = function() {
-    if(req.readyState === 4)
-    {
-      if(req.status === 200)
-      {
-        console.log(req.responseText);
-      }
-    }
+function ajax(url, callback, data, x) {
+  try {
+    x = new(this.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+    x.open(data ? 'POST' : 'GET', url, 1);
+    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    x.onreadystatechange = function () {
+      x.readyState > 3 && callback && callback(x.responseText, x);
+    };
+    x.send(data)
+  } catch (e) {
+    window.console && console.log(e);
   }
-  req.send();
+};
+
+function done(responseText){
+
+  console.log('done:', responseText)
+}
+
+function load(){
+  console.log('load');
+
+  ajax('dummy.json', done );
+  // // var request = new XMLHttpRequest();
+  // //
+  // // request.open('GET', 'http://1boon.kakao.com/index.html', true);
+  // // // console.log(request);
+  // //
+  // // request.onreadystatechange = function() {
+  // //   if(request.readyState === 4) {
+  // //     if(request.status === 200){
+  // //
+  // //       console.log(request.responseText)
+  // //     }
+  // //   }
+  // // }
+  // //
+  // // request.send();
+  // console.log('send')
+
 }
---
+++ 05/quiz/quiz.js
@@ -0,0 +1,25 @@
+var url = 'http://movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20170801&endDate=20171013&pageSize=3&pageNo=';
+//
+
+var info = document.getElementById('info')
+var str = '';
+
+getJSON(url, done);
+
+function done(result){
+
+  console.log(result);
+
+
+  for(var i=0; i < result.data.length; i++){
+
+    console.log(result.data[i].titleKo)
+    // str += '<h3>'+result.data[i].
+    str += '<h2>'+result.data[i].titleKo+'</h2>'
+    str += '<h3>'+result.data[i].rank.ranking+'위'+'</h3>'
+    str += '<h3>'+result.data[i].moviePoint.inspectPointAvg+'점'+'</h3>'
+    str += '<img src='+result.data[i].photo.fullname+' width="100">'
+  }
+
+  info.innerHTML = str;
+}
