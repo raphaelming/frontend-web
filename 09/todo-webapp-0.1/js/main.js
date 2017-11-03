85f285c84a4703913f68ff3a155a3e0f5868185b
Lming1 <spear131121@gmail.com>
2017-11-03 17:40:51 +0900

    리팩토링

--- 09/todo-webapp-0.1/js/main.js
+++ 09/todo-webapp-0.1/js/main.js
@@ -1,38 +1,34 @@
-//start...
 
-//개발자도구에서 메세지확인
-// console.log('todos.js');
+(function($, global, app){
 
-
-(function($, global){
 
   var $todoStringField = $('#todoString');
   var $listDom = $('#todoList');
 
 
   // todoStringField.addEventListener('keyup', addTodo);
-  $todoStringField.on('keyup', addTodo );
+  $todoStringField.on('keyup', app.view.addTodo );
 
 
   // //삭제버튼 이벤트 잡기 위해 상위에서 이벤트 listen 하기
-  // listDom.addEventListener('click', checkDelete);
-
   $listDom.on('click', '.delete', function(event){
 
-    console.log('delete')
+    //console.log('delete');
 
     var $deleteBtn = $(event.target);
+    var id = $deleteBtn.parent().data('id');
 
-    $deleteBtn.parent().remove();
-
-
-    saveData( $listDom.html() );
+    app.collection.remove( id );
 
   });
 
   // //처음 로딩시에 기존에 저장된 데이터 가져와서 보여주기
-  // listDom.innerHTML = loadData();
-  $listDom.html( loadData() );
+  var initData = app.util.storage.load();
+  if(initData) {
+    console.log(initData)
+    app.collection.set( initData );
+  }
 
 
-})(jQuery, window);
+
+})(jQuery, window, Todo);
--- 09/todo-webapp-0.1/js/util.js
+++ 09/todo-webapp-0.1/js/util.js
@@ -1,67 +1,28 @@
 
 
-function addTodo(event) {
 
-  console.log('todoStringField keyup!');
+(function(app) {
 
+  app.util = {
+    uniqId: function() {
+      return new Date().getTime();
+    },
+    storage : {
+      load: function () {
+        console.log('storage.load()');
+        return JSON.parse(localStorage.getItem(app.storageKey) || "[]");
+      },
+      save: function (event, data) {
+        console.log('storage.save()');
+        localStorage.setItem(app.storageKey, JSON.stringify(data));
 
-  var $field = $(event.currentTarget);
-  var fieldValue = $field.val();
-
-  if (event.keyCode !== 13 || fieldValue === "") {
-    console.log('event stop')
-    return false;
-    // event.stopPropagation(); //이벤트 전파중지
-    // return;
-  }
-
-
-  //엔터키가 아니면 함수 중지
-  // if (event.keyCode !== 13 || todoStringField.value === "") {
-  //   event.stopPropagation(); //이벤트 전파중지
-  //   return;
-  // }
-  //
-
-  // var newTodo = fieldValue;
-
-  $field.val('');
-  console.log('new​ todo:', fieldValue)
+      }
+    }
+  };
 
 
-  // var newTodo = todoStringField.value;
-  // todoStringField.value = "";
-  //
-  // console.log('새로운 todo:', newTodo);
-  //
+  app.$wrap.on('addCollection', app.util.storage.save);
+  app.$wrap.on('removeCollection', app.util.storage.save);
 
-  //TODO. 나중에 밖으로 빼기
-  var todoTemplateHtml = $('#todoTemplate').html();
-  var $listDom = $('#todoList');
+})(Todo);
 
-  $listDom.append( tmpl(todoTemplateHtml, {todo: fieldValue}) );
-
-  // listDom.innerHTML += tmpl(todoTemplateHtml, {todo: newTodo});
-
-
-  saveData($listDom.html());
-}
-
-function checkDelete(event) {
-  if(event.target.className !== 'delete') {
-    return;
-  }
-  var deleteBtn = event.target;
-  deleteBtn.parentElement.remove();
-
-  saveData();
-}
-
-function loadData() {
-  console.log('load');
-  return localStorage.getItem('todoHtml');
-}
-function saveData(html) {
-  console.log('save');
-  localStorage.setItem('todoHtml', html);
-}
