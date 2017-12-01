02616326956e28916e409e3368dc5cd3d24ea2a6
Lming1 <spear131121@gmail.com>
2017-12-01 17:08:29 +0900

    geo

---
+++ 14/full.js
@@ -0,0 +1,8 @@
+function fullScreen(){
+  if (screenfull.enabled) {
+  	screenfull.request( document.querySelector('img') );
+  }
+
+}
+
+document.getElementById('fullBtn').addEventListener('click', fullScreen);
